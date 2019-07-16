5ffc947c036f6e07dd64e799573a6be6605d8b26
Igor Cesar <igormarracho01@gmail.com>
2019-07-16 01:55:09 -0300

    making final config on refactor/structure and ading simple logic to login

--- src/client/pages/Home/Home.js
+++ src/client/pages/Home/Home.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import gql from 'graphql-tag';
 import { Layout } from 'antd';
 
 import LoginForm from '../../components/LoginForm';
 import _s from './Home.less';
-// import { Query, Mutation } from 'react-apollo';
 
 const { Header, Footer, Content } = Layout;
 
@@ -19,61 +17,3 @@ const HomePage = () => (
 );
 
 export default HomePage;
-
-// const GET_USERS = gql`
-//   query getUsers {
-//     users {
-//       id
-//     }
-//   }
-// `;
-
-// const LOG_IN = gql`
-//   mutation LogIn($email: String!, $password: String!) {
-//     LogIn(email: $email, password: $password) {
-//       id
-//     }
-//   }
-// `;
-
-// let input;
-// let input2;
-// <Mutation mutation={LOG_IN}>
-//   {LogIn => (
-//     <div>
-//       <form
-//         onSubmit={(e) => {
-//           e.preventDefault();
-//           LogIn({ variables: { email: input.value, password: input2.value } });
-//           input.value = '';
-//         }}
-//       >
-//         <input
-//           ref={(node) => {
-//             input = node;
-//           }}
-//         />
-//         <input
-//           ref={(node) => {
-//             input2 = node;
-//           }}
-//         />
-//         <button type="submit">Add Todo</button>
-//       </form>
-//     </div>
-//   )}
-// </Mutation>
-// <Query query={GET_USERS}>
-//   {({ loading, error, data }) => {
-//     if (loading) return 'Loading...';
-//     if (error) return `Error! ${error.message}`;
-
-//     return (
-//       <div>
-//         {data.users.map(user => (
-//           <p key={user.id}>{user.id}</p>
-//         ))}
-//       </div>
-//     );
-//   }}
-// </Query>
