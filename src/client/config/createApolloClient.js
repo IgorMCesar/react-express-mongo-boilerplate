b24a559e665fa1b674672b0572d4a3e0ba146ea4
IgorMCesar <igormarracho01@gmail.com>
2019-07-16 23:22:34 -0300

    fixing credential on client

--- src/client/config/createApolloClient.js
+++ src/client/config/createApolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql'
+  uri: 'http://localhost:8080/graphql',
+  credentials: 'include'
 });
 
 export default client;
