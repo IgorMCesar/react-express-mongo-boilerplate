40664001ff14e5932e7e7bc5e9626384bd047cc1
Igor Cesar <igormarracho01@gmail.com>
2019-08-11 13:26:06 -0300

    adding a component to deal with form input fields and fixing form related issues

---
+++ src/client/components/FormInputField/FormInputField.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Form } from 'antd';
+
+export const FormInputField = ({
+  field, // { name, value, onChange, onBlur }
+  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  InputType,
+  innerData,
+  hasFeedback,
+  ...props
+}) => {
+  const errorMessage = touched[field.name] && errors[field.name];
+  let inputStatus;
+
+  if (errorMessage) {
+    inputStatus = 'error';
+  } else if (touched[field.name] && field.value) {
+    inputStatus = 'success';
+  }
+
+  return (
+    <Form.Item help={errorMessage} validateStatus={inputStatus} hasFeedback={hasFeedback}>
+      <InputType {...field} {...props}>
+        {innerData}
+      </InputType>
+    </Form.Item>
+  );
+};
